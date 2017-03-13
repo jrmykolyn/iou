9969a1029bf8256d4c51e62bde42c9006ebfe9c8
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-13 09:06:59 -0400

    Added '.then()' method to 'IOU' prototype; updated constructor.

--- src/iou.js
+++ src/iou.js
@@ -1,8 +1,28 @@
 var IOU = ( function() {
+    // --------------------------------------------------
+    // CONSTRUCTOR
+    // --------------------------------------------------
     function IOU( callback ) {
+        if ( !callback ) {
+            throw new Error( '`IOU()` must be invoked with a `callback` argument' );
+        }
+
+        this.callback = callback;
+
         return this;
     } // /IOU()
 
 
+    // --------------------------------------------------
+    // INSTANCE METHODS
+    // --------------------------------------------------
+    IOU.prototype.then = function( onSuccess, onError ) {
+        this.callback( onSuccess, onError );
+    }
+
+
+    // --------------------------------------------------
+    // PUBLIC API
+    // --------------------------------------------------
     return IOU;
 } )();
